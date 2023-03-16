87793c48843ee005ccc5a3837c5295aebfbeff82
LouisDelarbre <41839095+LouisDelarbre@users.noreply.github.com>
2023-03-16 09:07:48 +0100

    optimize the code and allow "no square" matrix

--- ex6_rotate_matrix.py
+++ ex6_rotate_matrix.py
@@ -1,31 +1,48 @@
 import copy
 
 
-def rotate_matrix_once(matrix,direction):
-    new_matrix = copy.deepcopy(matrix)
+def size_matrix(matrix):
+    row = len(matrix)
+    cols = len(matrix[0])
+    return (row,cols)
 
-    lng_mat = len(matrix) - 1
+
+def rotate_matrix_once(matrix,direction):
+    size_matrix_var = size_matrix(matrix)
+    
+    #switching number of column and rows because it's doing it for each rotation
+    new_matrix = [[0 for j in range(size_matrix_var[0])] for i in range(size_matrix_var[1])]
+
+
+
     for index_i,i in enumerate(matrix):
         for index_j,j in enumerate(i) :                
             if direction == "R":
-                new_matrix[index_j][lng_mat-index_i] = matrix[index_i][index_j]
+                new_matrix[index_j][(size_matrix_var[0]-1)-index_i] = matrix[index_i][index_j]
             elif direction == "L":
-                new_matrix[lng_mat-index_j][index_i] = matrix[index_i][index_j]
+                new_matrix[(size_matrix_var[1]-1)-index_j][index_i] = matrix[index_i][index_j]
     return(new_matrix)
 
 
 def rotate_matrix(matrix, num_rotation, direction):
+    # to avoid useless rotation ( 4 * 90 = 360, so each 4 rotation means the final matrix is the same than the beginning)
+    num_rotation = num_rotation%4
     
-    for _ in range(num_rotation):
-        matrix = rotate_matrix_once(matrix,direction)
+    if num_rotation != 0:
+        for _ in range(num_rotation):
+            matrix = rotate_matrix_once(matrix,direction)
 
     print(matrix)
         
-        
 
-matrix = [
-    [1,2,3],
-    [1,3,5],
-    [1,4,7]
-]
-rotate_matrix(matrix,1, "L")
+
+
+
+if __name__ == '__main__':
+    matrix = [
+        [1,2,3,4,5],
+        [1,3,5,8,6],
+        [1,4,7,9,7],
+    ]
+
+    rotate_matrix(matrix,3, "L")
