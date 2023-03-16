b9db16ea7d1e28652263e291a043e3bba2fdc2b2
LouisDelarbre <41839095+LouisDelarbre@users.noreply.github.com>
2023-03-16 09:03:55 +0100

    add recursive function and creating_list_function
    add recursive function to the sum and and a function to create random list from a length entered as parameter.

--- ex5_mysum.py
+++ ex5_mysum.py
@@ -1,27 +1,32 @@
 import random
 
+
+len_list = 15
 list_num = []
 
-#creating the random list
-for i in range(10):
+#creating the random list with n elements ( n = len_list)
+for i in range(len_list):
     number = random.randint(1,100)
     list_num.append(number)
 
+#check if the list is composed with number and nothing else
 def is_array_number(list_numbers):
     if isinstance(list_numbers, list) and all(isinstance(number, int) for number in list_numbers):
         return True
     else:
         return False
 
+#recursive funct to have the sum from list_numbers
 def my_sum(list_numbers):
+    if len(list_numbers) > 0:
+        return list_numbers[-1] + my_sum(list_numbers[:-1])
+    else :
+        return 0
+        
+
+
+if __name__ == '__main__':
     #print to see the list a first time
-    print(list_numbers)
-    sum = 0
-    for i in list_numbers:
+    print(list_num)
 
-        sum += i
-
-    return sum
-
-
-print(my_sum(list_num))
+    print(f'Final sum = {my_sum(list_num)}')
